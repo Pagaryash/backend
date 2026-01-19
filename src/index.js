27fd0d0e5b18b11e2f9c2cd55d3d43f6b7827373
Pagaryash <yashpagar34@gmail.com>
2026-01-19 23:40:12 +0530

    User and Video Model

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 //require("dotenv").config({ path: "./.env" });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js"; // Added by me only
 dotenv.config({
   path: "./.env",
 });
@@ -15,6 +15,8 @@ connectDB()
   .catch((error) => {
     console.log("MONGO db connection failed !!! ", error);
   });
+
+//
 /*
 import express from "express";
 const app = express();
