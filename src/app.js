f4f7d2ecab19161f4df9342debb03cd5f0cdc4b6
Pagaryash <yashpagar34@gmail.com>
2026-01-20 14:48:52 +0530

    Routes and Controllers Basics

--- src/app.js
+++ src/app.js
@@ -16,4 +16,11 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter); // this is a syntax which we need to use for routers
+//http://localhost:8000/api/v1/users/register this register is in routes file
+
 export { app };
