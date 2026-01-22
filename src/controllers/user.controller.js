4d94d95815ee8b34402e611fccc2de41181c2d00
Pagaryash <yashpagar34@gmail.com>
2026-01-22 15:30:41 +0530

    Spelling Mistake and await

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -23,21 +23,21 @@ const registerUser = asyncHandler(async (req, res) => {
   if ([fullName, email, username, password].some(() => filed?.trim() === "")) {
     throw new ApiError(400, "All fields are required");
   }
-  const exsistedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
-  if (exsistedUser) {
+  if (existedUser) {
     throw new ApiError(409, "User with email or username");
   }
   const avatarLocalPath = req.files?.avatar[0]?.path;
   const coverImageLocalPath = req.files?.coverImage[0]?.path;
   if (!avatarLocalPath) {
-    throw new ApiError(400, "Avatar file is required");
+    throw new ApiError(400, "Avatar local file is required");
   }
   const avatar = await uploadOnCloudinary(avatarLocalPath);
   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
   if (!avatar) {
-    throw new ApiError(400, "Avatar file is required");
+    throw new ApiError(400, "Avatar cloud file is required");
   }
   const user = await User.create({
     fullName,
