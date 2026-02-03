fe6dc537e12108a43d2f06cb12510412e34db8cf
Pagaryash <yashpagar34@gmail.com>
2026-02-03 11:01:57 +0530

    Fixed User Controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -138,11 +138,11 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
-  await User.findByIdAndDelete(
+  await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refreshToken: undefined,
+      $unset: {
+        refreshToken: 1, // this removes the field from document
       },
     },
     {
